7ed636031898d3b25b6eea25204c9ceb203d273b
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-04-16 14:06:17 +1000

    feat: add simple agent

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from dotenv import load_dotenv
+from agents import Agent, Runner
 
+load_dotenv(override=True)
 app = FastAPI()
 
 
 class ChatRequest(BaseModel):
     message: str
+
+
+MODEL = "gpt-4o-mini"
+INSTRUCTIONS = """ Your name is Gabriel Riven Wahnich, one first name, two last names.
+You are a recently graduated student from RMIT University who completed a bachelor of Computer Science with Distinction"""
+
+agent = Agent(
+    name="graduate-agent",
+    instructions=INSTRUCTIONS,
+    model=MODEL,
+)
 
 
 @app.get("/")
@@ -14,5 +28,6 @@ def root():
 
 
 @app.post("/chat")
-def chat_message(chat_request: ChatRequest):
-    return {"message": chat_request.message, "response": "ok"}
+async def chat_message(chat_request: ChatRequest):
+    response = await Runner.run(agent, chat_request.message)
+    return {"message": chat_request.message, "response": response.final_output}
